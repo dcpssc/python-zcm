78ba09b43919a15c0d4c8228e496f3aaee9ed64f
Scott <eiselesr@gmail.com>
2016-07-20 17:19:17 -0500

    playing with creating a component

---
+++ tests/cities-skylines/traffic_light_controller.py
@@ -0,0 +1,10 @@
+from zcm import *
+
+class Traffic_light_controller(object):
+    """docstring for Traffic_light_controller"""
+    def __init__(self, arg):
+        super(Traffic_light_controller, self).__init__()
+        self.arg = arg
+
+    def getQ(self):
+        
